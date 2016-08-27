ccaf38e6cdb1fa05c32ea879abe50752035ff2db
Tom Jaejoon Lee <serialkk@naver.com>
2016-08-27 12:09:15 +0900

    add P082_AseBox

---
+++ P082_AseBox/P082_AseBox/AseBox/Sample.cpp
@@ -0,0 +1,13 @@
+#include "Sample.h"
+
+
+
+Sample::Sample()
+{
+}
+
+
+Sample::~Sample()
+{
+}
+GBASIS_RUN(L"Ase Box");
---
+++ P082_AseBox/P082_AseBox/AseBox/Sample.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "GBasisLib_0.h"
+
+
+class Sample : public GBASISLib_0
+{
+public:
+	Sample();
+	virtual ~Sample();
+};
+

86641b55c5daf1068133dde25ebd6c3def190aad
Tom Jaejoon Lee <serialkk@naver.com>
2016-10-04 17:14:30 +0900

    add GbsViewer and GbsViewer. update GbsExport2016

--- gbsExport2016/GbsWriter.cpp
+++ gbsExport2016/GbsWriter.cpp
@@ -748,6 +748,9 @@ bool GbsWriter::ExpObject() {
 			m_gObjectList[iObj].nodeName,
 			m_gObjectList[iObj].nodeParentName);
 
+		_ftprintf(m_fp, _T("%d\n"),
+			m_gObjectList[iObj].iRef);
+
 		_ftprintf(m_fp, _T("%s %10.4f %10.4f %10.4f %10.4f  %10.4f %10.4f %10.4f %10.4f  %10.4f %10.4f %10.4f %10.4f  %10.4f %10.4f %10.4f %10.4f \n"),
 			_T("#WORLD"),
 			m_gObjectList[iObj].matWorld._11,
@@ -774,7 +777,7 @@ bool GbsWriter::ExpObject() {
 		std::sort(m_gObjectList[iObj].triList.begin(),
 			m_gObjectList[iObj].triList.end(),
 			AScendingSort());
-		_ftprintf(m_fp, _T("%s %d\n"), _T("TRIANGLE"),
+		_ftprintf(m_fp, _T("%s %d\n"), _T("#TRIANGLE"),
 			m_gMtlList[m_gObjectList[iObj].iRef].subMtls.size());
 
 		int iAddCount = 0;
